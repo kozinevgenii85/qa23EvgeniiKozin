dccca8c18d8882849191d531d515426d216e3cca
kozinevgenii85 <58528854+kozinevgenii85@users.noreply.github.com>
2020-04-21 19:10:21 +0300

    Added Refactor not push metod SerchGoogle

--- First_Selenium_Project/src/test/java/com/telran/qa/tests/OpenGoogleTest.java
+++ First_Selenium_Project/src/test/java/com/telran/qa/tests/OpenGoogleTest.java
@@ -1,6 +1,5 @@
 package com.telran.qa.tests;
 
-//import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -14,7 +13,6 @@ WebDriver wd;
 
 @BeforeClass
 public void setUp(){
-    //WebDriverManager.chromedriver().setup();
     wd = new ChromeDriver();
     wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     wd.get("https://www.google.com/");
@@ -25,17 +23,30 @@ public void setUp(){
 @Test
 public void testOpenGoogle(){
 
-    wd.findElement(By.id("gbwa")).click();
-    wd.findElement(By.id("gbwa")).click();
+    click(By.className("gb_D"));
+    click(By.className("gb_D"));
 }
+
+    private void click(By locator) {
+        wd.findElement(locator).click();
+    }
 
     @Test
     public void testSearchGoogle() throws InterruptedException {
-        wd.findElement(By.name("q")).click();
-        wd.findElement(By.name("q")).clear();
-        wd.findElement(By.name("q")).sendKeys("yandex");
-        wd.findElement(By.className("gNO89b")).click();
+        click(By.name("q"));
+        clear(By.name("q"));
+        wd.findElement(By.name("q")).sendKeys("Java");
         Thread.sleep(3000);
+    }
+
+    private void clear(By locator) {
+        wd.findElement(locator).clear();
+    }
+
+    @Test
+    public void testOpenProfile() {
+        click(By.id("gb_70"));
+
     }
 
 @AfterClass
